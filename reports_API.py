1868d49083fe3925901495f2ae7e3aba3cf8f178
Dave <nightjars@gmail.com>
2018-05-20 13:21:49 -0700

    .

--- reports_API.py
+++ reports_API.py
@@ -11,10 +11,12 @@ import camera_API
 
 reports_api = flask.Blueprint('reports_api', __name__)
 
+
 @reports_api.route('/api/vehicle/report_by_freq')
 @flask_jwt.jwt_required()
 def get_report_by_freq():
     report = []
     for plate in flask_plate.db.event_collection.find().distinct('plate'):
         report.append((plate, flask_plate.db.event_collection.find({'plate': plate}).count()))
-    return report
+    response = flask.jsonify({'results': report})
+    return response
